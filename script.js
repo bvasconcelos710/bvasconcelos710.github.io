d28e00c6b6de628f5586055a5a2a7d58b8229a25
Bruno Vasconcelos <bvasconcelos710@gmail.com>
2024-06-27 18:07:15 -0300

    Excluindo arquivos antigos

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector("button");
-
-function alterarTema() {
-    document.body.classList.toggle("dark");
-}
-
-btn.addEventListener("click", () => {
-    alterarTema();
-})
